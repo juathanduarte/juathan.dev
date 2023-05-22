4adaf85db8e2bebe2fcaba07efc8bb01e838c9ea
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2023-05-22 15:39:33 -0300

    feat: add global style

---
+++ src/styles/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        border: 0;
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        outline: none;
+        font-size: 16px;
+        font-weight: 400;
+
+    }
+`
+
+export default GlobalStyle;
